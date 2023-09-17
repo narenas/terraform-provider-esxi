b784e70104285c33d32fd0892cef80cf794a2f82
Nicolas Arenas <nicolas.arenas@gmail.com>
2023-09-17 20:37:42 +0200

    Allow extra-config in case nested_esxi set to true

--- esxi/guest-create.go
+++ esxi/guest-create.go
@@ -255,7 +255,9 @@ func guestCREATE(c *Config, guest_name string, disk_store string,
 			}
 			log.Println("[guestCREATE] ovf_properties extra_params: " + extra_params)
 		}
-
+		if ( (nested_esxi == "y") && (is_ovf_properties == false) ) {
+			extra_params = "--allowExtraConfig "
+		} 
 		ovf_cmd := fmt.Sprintf("ovftool --acceptAllEulas --noSSLVerify --X:useMacNaming=false %s "+
 			"-dm=%s --name='%s' --overwrite -ds='%s' %s '%s' '%s'", extra_params, boot_disk_type, guest_name, disk_store, net_param, src_path, dst_path)
 
